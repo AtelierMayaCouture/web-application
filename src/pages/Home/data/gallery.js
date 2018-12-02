a65239226901b319246435b536b3e5934ab3b953
Aksels Ledins <aksels.ledins@gmail.com>
2018-12-02 23:15:58 +0100

    add some pictures

--- src/pages/Home/data/gallery.js
+++ src/pages/Home/data/gallery.js
@@ -1,85 +1,109 @@
-
 const data = [
   {
-    key: 15,
-    src: 'robe.jpg',
-    alt: 'Robe',
+    key: "2018_1",
+    src: "robe_2018_1.jpg",
+    alt: "Création"
   },
   {
-    key: 6,
-    src: 'file-1460930053002.jpg',
-    alt: 'Echarpes',
+    key: "2018_2",
+    src: "robe_2018_2.jpg",
+    alt: "Création"
+  },
+  {
+    key: "2018_3",
+    src: "robe_2018_3.jpg",
+    alt: "Création"
+  },
+  {
+    key: "2018_4",
+    src: "robe_2018_4.jpg",
+    alt: "Création"
+  },
+  {
+    key: "2018_5",
+    src: "robe_2018_5.jpg",
+    alt: "Création"
+  },
+  {
+    key: 15,
+    src: "robe.jpg",
+    alt: "Robe"
   },
   {
     key: 1,
-    src: 'file-1460929733088.jpg',
-    alt: 'Sac à tarte',
+    src: "file-1460929733088.jpg",
+    alt: "Sac à tarte"
   },
   {
     key: 10,
-    src: 'haut.jpg',
-    alt: 'Haut',
+    src: "haut.jpg",
+    alt: "Haut"
   },
   {
     key: 0,
-    src: 'file-1460929452987.jpg',
-    alt: 'Haut',
+    src: "file-1460929452987.jpg",
+    alt: "Haut"
   },
   {
     key: 2,
-    src: 'file-1460929981900.jpg',
-    alt: 'Sac',
+    src: "file-1460929981900.jpg",
+    alt: "Sac"
   },
   {
     key: 3,
-    src: 'file-1460929423580.jpg',
-    alt: 'Haut',
+    src: "file-1460929423580.jpg",
+    alt: "Haut"
   },
   {
     key: 5,
-    src: 'file-1460929885504.jpg',
-    alt: 'Veste',
+    src: "file-1460929885504.jpg",
+    alt: "Veste"
   },
   {
     key: 7,
-    src: 'gilet-1.jpg',
-    alt: 'Gilet',
+    src: "gilet-1.jpg",
+    alt: "Gilet"
   },
   {
     key: 8,
-    src: 'gilet-historique.jpg',
-    alt: 'Gilet historique',
+    src: "gilet-historique.jpg",
+    alt: "Gilet historique"
   },
   {
     key: 9,
-    src: 'haut-3.jpg',
-    alt: 'Haut',
+    src: "haut-3.jpg",
+    alt: "Haut"
   },
   {
     key: 11,
-    src: 'jupe.jpg',
-    alt: 'Jupe',
+    src: "jupe.jpg",
+    alt: "Jupe"
   },
   {
     key: 12,
-    src: 'napoleon.jpg',
-    alt: 'Napoleon',
+    src: "napoleon.jpg",
+    alt: "Napoleon"
   },
   {
     key: 13,
-    src: 'robe-2.jpg',
-    alt: 'Robe',
+    src: "robe-2.jpg",
+    alt: "Robe"
   },
   {
     key: 14,
-    src: 'robe-3.jpg',
-    alt: 'Robe',
+    src: "robe-3.jpg",
+    alt: "Robe"
   },
   {
     key: 16,
-    src: 'veste-bretonne.jpg',
-    alt: 'Vest bretonne',
+    src: "veste-bretonne.jpg",
+    alt: "Vest bretonne"
   },
+  {
+    key: 6,
+    src: "file-1460930053002.jpg",
+    alt: "Echarpes"
+  }
 ];
 
 export default data;
