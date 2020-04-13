aa6e3dd89c61a0c67c5dc2644c1b34ef7b0b6bde
Aksels Ledins <aksels.ledins@gmail.com>
2020-04-13 19:15:12 +0800

    coronavirus update

--- src/pages/Home/FullWidthSlider/HeroContent/index.js
+++ src/pages/Home/FullWidthSlider/HeroContent/index.js
@@ -1,5 +1,5 @@
-import React, { PropTypes as T } from 'react';
-import pure from 'recompose/pure';
+import React, { PropTypes as T } from "react";
+import pure from "recompose/pure";
 
 const HeroContent = ({ text, img }) => (
   <div className="home-content container">
@@ -9,16 +9,30 @@ const HeroContent = ({ text, img }) => (
           <div className="col-sm-6 col-sm-offset-3 col-xs-8 col-xs-offset-2">
             <img src={img} alt="Logo" />
           </div>
-          </div>
-          <h2 className="hs-line-3 mb-0">{text}</h2>
-          </div>
+        </div>
+        <h2 className="hs-line-3 mb-0">{text}</h2>
+        <h3>
+          <u>Coronavirus</u>
+          <ul>
+            <li>
+              Je vous propose des <b>masques</b> pour vous protéger. Ils suivent
+              les conseils de fabrication du CHU de Grenoble.
+            </li>
+            <li>
+              Je peux me déplacer chez vous en gardant les distances de sécurité
+              et vous pouvez me confier les travaux de couture en les mettant
+              dans un sac.
+            </li>
+          </ul>
+        </h3>
       </div>
+    </div>
   </div>
 );
 
 HeroContent.propTypes = {
   text: T.string.isRequired,
   img: T.string.isRequired,
-}
+};
 
 export default pure(HeroContent);
