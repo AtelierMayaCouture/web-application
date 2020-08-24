62d0c111b4f98b1959d9f4958a884f81031b1e1b
Aksels Ledins <aksels.ledins@gmail.com>
2020-08-24 10:22:55 +0800

    maj tarifs

--- src/pages/Tarifs/components/Autres/index.js
+++ src/pages/Tarifs/components/Autres/index.js
@@ -1,28 +1,28 @@
-import React from 'react';
-import pure from 'recompose/pure';
+import React from "react";
+import pure from "recompose/pure";
 
 const Autres = () => (
   <span>
-    <h2 className="section-title font-alt mb-70 mb-sm-40">Autres prestations</h2>
+    <h2 className="section-title font-alt mb-70 mb-sm-40">
+      Autres prestations
+    </h2>
 
     <div className="section-text mb-80 mb-xs-60">
-        <div className="row">
-          <div className="col-sm-3" />
-            <div className="col-sm-6">
-                <table className="table table-hover">
-                  <tbody>
-                    <tr>
-                        <td>
-                            Créations sur mesure, autres demandes
-                        </td>
-                        <td className="align-right">26€ / h</td>
-                    </tr>
-                  </tbody>
-                </table>
-            </div>
-            <div className="col-sm-3" />
+      <div className="row">
+        <div className="col-sm-3" />
+        <div className="col-sm-6">
+          <table className="table table-hover">
+            <tbody>
+              <tr>
+                <td>Créations sur mesure, autres demandes</td>
+                <td className="align-right">28€ / h</td>
+              </tr>
+            </tbody>
+          </table>
         </div>
+        <div className="col-sm-3" />
       </div>
+    </div>
   </span>
 );
 
--- src/pages/Tarifs/components/Cours/index.js
+++ src/pages/Tarifs/components/Cours/index.js
@@ -1,30 +1,44 @@
-import React from 'react';
-import pure from 'recompose/pure';
+import React from "react";
+import pure from "recompose/pure";
 
 const Cours = () => (
   <span>
     <h2 className="section-title font-alt mb-70 mb-sm-40">Cours de couture</h2>
     <div className="section-text mb-80 mb-xs-60">
-        <div className="row">
-          <div className="col-sm-3" />
-            <div className="col-sm-6">
-                <table className="table table-hover">
-                  <tbody>
-                    <tr>
-                        <td>
-                            Les 3 h
-                            <div className="small">
-                                Le samedi - Jusqu'a 4 personnes simultanément
-                            </div>
-                        </td>
-                        <td className="align-right">24€</td>
-                    </tr>
-                  </tbody>
-                </table>
-            </div>
-            <div className="col-sm-3" />
+      <div className="row">
+        <div className="col-sm-3" />
+        <div className="col-sm-6">
+          <table className="table table-hover">
+            <tbody>
+              <tr>
+                <td>Les 3h</td>
+                <td className="align-right">28€</td>
+              </tr>
+
+              <tr>
+                <td>Les 10 séances de 3h</td>
+                <td className="align-right">250€</td>
+              </tr>
+
+              <tr>
+                <td>
+                  Quand?
+                  <div className="small">
+                    <br />
+                    <div>Samedi de 9h30 à 12h30</div>
+                    <div>Jeudi 17h à 20h</div>
+                    <br />
+                    Jusqu'a 4 personnes simultanément.
+                  </div>
+                </td>
+                <td></td>
+              </tr>
+            </tbody>
+          </table>
         </div>
+        <div className="col-sm-3" />
       </div>
+    </div>
   </span>
 );
 
