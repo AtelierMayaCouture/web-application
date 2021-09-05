e2cafe3db96749b8a34b3ba2bc4e0d8368d312e8
Aksels Ledins <aksels.ledins@gmail.com>
2021-09-05 19:33:48 +0200

    update hours

--- src/pages/Horaires/data/index.js
+++ src/pages/Horaires/data/index.js
@@ -1,11 +1,11 @@
 
 const data = [
-  { day: 'Lundi', hours: '9h - 12:15, 14h - 16h45' },
-  { day: 'Mardi', hours: '9h - 12:15, 15h - 18h30' },
-  { day: 'Mercredi', hours: '9h - 12:15, 15h - 18h30' },
-  { day: 'Jeudi', hours: '9h - 12:15, 14h - 16h45' },
-  { day: 'Vendredi', hours: '9h - 12:15, 15h - 18h30' },
-  { day: 'Samedi', hours: '9h30 - 12:30' },
+  { day: 'Lundi', hours: 'Fermé' },
+  { day: 'Mardi', hours: '9h - 12h15, 15h - 18h30' },
+  { day: 'Mercredi', hours: '9h - 12h30, 15h - 18h30' },
+  { day: 'Jeudi', hours: '9h - 12h15, 15h - 18h30' },
+  { day: 'Vendredi', hours: '9h - 12h30, 15h - 18h30' },
+  { day: 'Samedi', hours: '9h30 - 12h30' },
   { day: 'Dimanche', hours: 'Fermé' },
 ];
 
