2a0468bc7916694c3ba8a1e872a40f03340ddf88
Aksels LEDINS <aksels.ledins@gmail.com>
2023-09-04 20:13:12 +0800

    update horaires

--- src/pages/Horaires/data/index.js
+++ src/pages/Horaires/data/index.js
@@ -1,13 +1,11 @@
-
-
 const data = [
-  { day: 'Lundi', hours: 'Fermé' },
-  { day: 'Mardi', hours: '9h30 - 12h30' },
-  { day: 'Mercredi', hours: '9h30 - 12h30' },
-  { day: 'Jeudi', hours: '9h30 - 12h30' },
-  { day: 'Vendredi', hours: '9h30 - 12h30' },
-  { day: 'Samedi', hours: '9h30 - 12h30' },
-  { day: 'Dimanche', hours: 'Fermé' },
+  { day: "Lundi", hours: "Fermé" },
+  { day: "Mardi", hours: "9h30 - 12h30" },
+  { day: "Mercredi", hours: "9h30 - 12h30 & 14h - 18h30" },
+  { day: "Jeudi", hours: "9h30 - 12h30" },
+  { day: "Vendredi", hours: "9h30 - 12h30" },
+  { day: "Samedi", hours: "9h30 - 12h30" },
+  { day: "Dimanche", hours: "Fermé" },
 ];
 
 /*
