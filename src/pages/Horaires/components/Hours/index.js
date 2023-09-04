07759098fc75106187a2866f1732a7480d0ffd17
Aksels LEDINS <aksels.ledins@gmail.com>
2023-09-04 20:16:24 +0800

    update horaires

--- src/pages/Horaires/components/Hours/index.js
+++ src/pages/Horaires/components/Hours/index.js
@@ -1,32 +1,37 @@
-import React from 'react';
-import pure from 'recompose/pure';
+import React from "react";
+import pure from "recompose/pure";
 
 // global components
-import Table from '../../../../components/Table';
+import Table from "../../../../components/Table";
 
 // local components
-import Line from './../Line'
+import Line from "./../Line";
 
 // data
-import data from './../../data';
+import data from "./../../data";
 
 const Hours = () => (
   <div className="section-text mb-80 mb-xs-60">
     <div className="row">
       <div className="col-sm-3" />
-        <div className="col-sm-6">
-          <p>L'atelier est ouvert du mardi au samedi de 9h30 à 12h30.</p>
-          <br /><br />
-          <Table>
-            { data.map( item =>
-              <Line
-                key={`${item.day}-${item.hours}`}
-                title={item.day}
-                description={item.hours} />
-            )}
-          </Table>
-        </div>
-        <div className="col-sm-3" />
+      <div className="col-sm-6">
+        <p>
+          L'atelier est ouvert du mardi au samedi de 9h30 à 12h30 et le mercredi
+          après-midi.
+        </p>
+        <br />
+        <br />
+        <Table>
+          {data.map((item) => (
+            <Line
+              key={`${item.day}-${item.hours}`}
+              title={item.day}
+              description={item.hours}
+            />
+          ))}
+        </Table>
+      </div>
+      <div className="col-sm-3" />
     </div>
   </div>
 );
